47c08a15dbde6327b712f76086c6a7a0bcca80ee
Aelto <Aelto@users.noreply.github.com>
2022-12-22 11:15:16 +0100

    update script to include all sharedutils

--- scripts/release.bat
+++ scripts/release.bat
@@ -12,12 +12,17 @@ mkdir "%modpath%\release"
 
 :: first the mods
 mkdir "%modpath%\release\mods\"
-XCOPY "%modpath%\mod_sharedutils_mappins\" "%modpath%\release\mods\mod_sharedutils_mappins\" /e /s /y
-XCOPY "%modpath%\mod_sharedutils_dialogChoices\" "%modpath%\release\mods\mod_sharedutils_dialogChoices\" /e /s /y
-XCOPY "%modpath%\mod_sharedutils_journalquest\" "%modpath%\release\mods\mod_sharedutils_journalquest\" /e /s /y
-XCOPY "%modpath%\mod_sharedutils_npcInteraction\" "%modpath%\release\mods\mod_sharedutils_npcInteraction\" /e /s /y
-XCOPY "%modpath%\mod_sharedutils_noticeboards\" "%modpath%\release\mods\mod_sharedutils_noticeboards\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_custombossbar\" "%modpath%\release\mods\mod_sharedutils_custombossbar\" /e /s /y
 XCOPY "%modpath%\mod_sharedutils_customcooldowns\" "%modpath%\release\mods\mod_sharedutils_customcooldowns\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_damagemodifiers\" "%modpath%\release\mods\mod_sharedutils_damagemodifiers\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_dialogChoices\" "%modpath%\release\mods\mod_sharedutils_dialogChoices\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_dialogHover\" "%modpath%\release\mods\mod_sharedutils_dialogHover\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_helpers\" "%modpath%\release\mods\mod_sharedutils_helpers\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_journalquest\" "%modpath%\release\mods\mod_sharedutils_journalquest\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_mappins\" "%modpath%\release\mods\mod_sharedutils_mappins\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_noticeboards\" "%modpath%\release\mods\mod_sharedutils_noticeboards\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_npcInteraction\" "%modpath%\release\mods\mod_sharedutils_npcInteraction\" /e /s /y
+XCOPY "%modpath%\mod_sharedutils_tiny_boostrapper\" "%modpath%\release\mods\mod_sharedutils_tiny_boostrapper\" /e /s /y
 XCOPY "%modpath%\mod0000_sharedutilsmappinsfhudpatch\" "%modpath%\release\mods\mod0000_sharedutilsmappinsfhudpatch\" /e /s /y
 
 :: then the dlcs
